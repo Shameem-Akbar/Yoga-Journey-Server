f00f3e08e5bbff7ca5b369b7800b6977ff100afd
Shameem-Akbar <Shameemakbar789@gmail.com>
2023-06-13 23:38:05 +0600

    payment intent api created

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -242,6 +243,21 @@ async function run() {
             const query = { _id: new ObjectId(id) };
             const result = await studentClassesCollection.deleteOne(query);
             res.send(result);
+        })
+
+        // create payment intent
+        app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+            const { price } = req.body;
+            const amount = parseInt(price * 100);
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+
+            res.send({
+                clientSecret: paymentIntent.client_secret
+            })
         })
 
 
